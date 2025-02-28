d24e29ec7213ab41805e1b659f55dac3ca0c7727
PAB333 <pabrd3@gmail.com>
2025-02-28 18:16:30 +0100

    client done

--- src/client.c
+++ src/client.c
@@ -6,15 +6,46 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 19:03:28 by pibreiss          #+#    #+#             */
-/*   Updated: 2025/02/27 19:30:14 by pibreiss         ###   ########.fr       */
+/*   Updated: 2025/02/28 17:59:52 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+void	char_to_binary(char c, char *binary)
+{
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		binary[7 - i] = '0' + ((c >> i) & 1);
+		i--;
+	}
+	binary[8] = '\0';
+}
+
 void	send_message(int PID, char *message)
 {
-	
+	int		i;
+	int		j;
+	char	binary[9];
+
+	i = 0;
+	while (message[i])
+	{
+		char_to_binary(message[i], binary);
+		j = 0;
+		while (binary[j])
+		{
+			if (binary[j] == '0')
+				kill(PID, SIGUSR1);
+			else if (binary[j] == '1')
+				kill(PID, SIGUSR2);
+			j++;
+		}
+		i++;
+	}
 }
 
 int	main(int ac, char **av)
--- src/server.c
+++ src/server.c
@@ -6,9 +6,14 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 19:03:18 by pibreiss          #+#    #+#             */
-/*   Updated: 2025/02/27 19:09:58 by pibreiss         ###   ########.fr       */
+/*   Updated: 2025/02/28 18:14:23 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+int	main(void)
+{
+	ft_printf("Welcome you are on the server\n");
+	ft_printf("The PID to connect from the client is : %d\n", getpid());
+}
